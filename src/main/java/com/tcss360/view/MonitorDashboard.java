bd2117e69af61cf10a8182d564d4e84a6ec92561
Logan <logan_black@aol.com>
2026-04-28 06:23:53 -0700

    implemented MonitorDashboard fields, method signatures, and javadoc

--- src/main/java/com/tcss360/view/MonitorDashboard.java
+++ src/main/java/com/tcss360/view/MonitorDashboard.java
@@ -1,8 +1,126 @@
+/*
+ * Course Project
+ * TCSS 360 Spring 2026
+ * Logan Black
+ */
+
 package com.tcss360.view;
 
+import java.util.ArrayList;
+
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+
+import com.tcss360.model.AnomalyRecord;
+import com.tcss360.model.Drone;
+
 /**
- * 
+ * The MonitorDashboard class is the GUI for human-system interaction
+ * @author Logan Black
+ * @version 28 APR 2026
  */
 public class MonitorDashboard {
 
+    /** The drone table */
+    private JTable myDroneTable;
+
+    /** The drone map panel */
+    private JPanel myMapPanel;
+
+    /** The alert log area */
+    private JTextArea myAlertLog;
+
+    /** The alert log query area */
+    private JPanel myQjeryPanel;
+
+    /**
+     * Constructor
+     */
+    public MonitorDashboard() {
+
+        /* Insert Logic Here */
+
+    }
+
+    /**
+     * 
+     * @param theDrones the drone fleet
+     */
+    public void display(ArrayList<Drone> theDrones) {
+
+        /* Insert Logic Here */
+        
+    }
+
+    /**
+     * 
+     * @param theRecord an anomaly record
+     */
+    public void addAlert(AnomalyRecord theRecord) {
+
+        /* Insert Logic Here */
+
+    }
+
+    /**
+     * 
+     * @param theDrones the drone fleet
+     */
+    private void updateDroneTelemetry(ArrayList<Drone> theDrones) {
+
+        /* Insert Logic Here */
+
+    }
+
+    /**
+     * 
+     * @param theDrones the drone fleet
+     */
+    private void paintDronePositions(ArrayList<Drone> theDrones) {
+
+        /* Insert Logic Here */
+
+    }
+
+    /**
+     * 
+     */
+    private void showQueryScreen() {
+
+        /* Insert Logic Here */
+
+    }
+
+    /**
+     * 
+     * @param theFilePath the file save path
+     */
+    private void exportAnomalyLogToCSV(String theFilePath) {
+
+        /* Insert Logic Here */
+
+    }
+
+    /**
+     * 
+     * @return
+     */
+    private JMenuBar buildMenuBar() {
+
+        /* Insert Logic Here */
+
+        return null;
+    }
+
+    /**
+     * 
+     */
+    private void handleExit() {
+
+        /* Insert Logic Here */
+
+
+    }
 }
