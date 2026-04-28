490c80820a70032a3262bef0fb7dd09c74ff6574
Logan <logan_black@aol.com>
2026-04-27 22:17:45 -0700

    initial commit

---
+++ src/main/java/com/tcss360/DroneMonitorApp.java
@@ -0,0 +1,15 @@
+package com.tcss360;
+
+/**
+ * 
+ */
+public class DroneMonitorApp {
+
+    /**
+     * 
+     * @param theArgs
+     */
+    public static void main( String[] theArgs ) {
+
+    }
+}
---
+++ src/test/java/com/tcss360/DroneMonitorAppTest.java
@@ -0,0 +1,19 @@
+package com.tcss360;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for DroneMonitorApp
+ */
+public class DroneMonitorAppTest 
+{
+    /**
+     * Test
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
